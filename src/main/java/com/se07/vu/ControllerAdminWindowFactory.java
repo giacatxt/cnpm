ad28f7b0d6759c6ffb0e70e50989ecfad0630181
kaisacht <105161581+kaisacht@users.noreply.github.com>
2022-11-29 21:36:56 +0700

    logout admin

--- src/main/java/com/se07/vu/ControllerAdminWindowFactory.java
+++ src/main/java/com/se07/vu/ControllerAdminWindowFactory.java
@@ -1,14 +1,27 @@
 package com.se07.vu;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
+import javafx.scene.control.ButtonType;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
 
 public class ControllerAdminWindowFactory {
     @FXML
-    ImageView ImageAdminLogout;
+    Button buttonLogoutAdmin;
     @FXML
-    ImageView ImageAdminHome;
-
+    BorderPane borderPaneAdmin;
+    Stage stage;
+    public void onButtonLogoutAdmin(){
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Đăng xuất");
+        alert.setHeaderText("Bạn thực sự muốn đăng xuất");
+        alert.setContentText("Bạn có muốn lưu trước khi thoát");
+        if(alert.showAndWait().get() == ButtonType.OK) {
+            stage = (Stage) borderPaneAdmin.getScene().getWindow();
+            System.out.println("Bạn đã đăng xuất khỏi trái đất");
+            stage.close();
+        }
+    }
 }
